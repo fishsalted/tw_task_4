5d00550b129aadd5c170fc70a28eaa9c634f216d
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-03-30 14:29:15 +0800

    完成 Flaten.java

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
+import java.util.*;
 
 public class Flaten {
 
@@ -12,10 +12,25 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for(Integer[] integer_list : array) {
+            result.addAll(Arrays.asList(integer_list));
+        }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> item_set = new TreeSet<>();
+        for(Integer[] integer_list : array) {
+            for(Integer number : integer_list) {
+                if(!item_set.contains(number)) {
+                    result.add(number);
+                    item_set.add(number);
+                }
+            }
+            item_set.addAll(Arrays.asList(integer_list));
+        }
+        return result;
     }
 }
